2659cc7c2e2709fe629bacc2611b53f4ba8b86cc
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-06-05 17:20:43 +0530

    1. Added styles in app.component.css. 2. Used string interpolation in ServerComponent.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html',
+  templateUrl: './server.component.html'
 })
-export class ServerComponent {}
+export class ServerComponent {
+  id = 10;
+  status = 'offline';
+
+  getServerStatus() {
+    return this.status;
+  }
+}
