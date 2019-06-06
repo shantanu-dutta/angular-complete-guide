103914bdeb568b9229d7883a0269b0a4377fd248
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-06-06 08:37:54 +0530

    Added ngStyle, ngClass in ServerComponent

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,13 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `
+  ]
 })
 export class ServerComponent {
   id = 10;
   status = 'offline';
 
+  constructor() {
+    this.status = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus() {
     return this.status;
+  }
+
+  getColor() {
+    return this.getServerStatus() === 'online' ? 'green' : 'red';
   }
 }
