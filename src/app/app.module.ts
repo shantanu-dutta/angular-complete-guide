ed4d2841d23f03bc52ed84c58670fced4ad806ff
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-06-06 08:12:05 +0530

    1. Added disabled button override styles in styles.css. 2. Added FormsModule in AppModule. 3. Updated code for ServersComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -7,7 +8,7 @@ import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [AppComponent, ServerComponent, ServersComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServers = false;
+  serverName = 'Test server';
+  serverCreated = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServers = true;
+    }, 2000);
   }
 
+  ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreated = true;
+  }
 }
