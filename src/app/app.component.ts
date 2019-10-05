2e7bd1f71e01713cc9257ef96d07b6b062008fc5
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-10-05 13:29:28 +0530

    Added animation trigger and states.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
+import { trigger, state, style } from '@angular/animations';
+
+export enum DivAnimationStates {
+  Normal = 'normal',
+  Highlighted = 'highlighted'
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state(
+        DivAnimationStates.Normal,
+        style({
+          'background-color': 'red',
+          transform: 'translateX(0)'
+        })
+      ),
+      state(
+        DivAnimationStates.Highlighted,
+        style({
+          'background-color': 'blue',
+          transform: 'translateX(100px)'
+        })
+      )
+    ])
+  ]
 })
 export class AppComponent {
+  state = DivAnimationStates.Normal;
   list = ['Milk', 'Sugar', 'Bread'];
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item: string) {
+    const itemIndex = this.list.indexOf(item);
+    this.list.splice(itemIndex, 1);
+  }
 }
